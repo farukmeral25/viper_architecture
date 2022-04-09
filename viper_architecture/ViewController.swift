6a0c050fa10ae1f7b69f550a882705824bdb0b56
farukmeral25 <farukmeral25@gmail.com>
2022-04-09 15:05:34 +0300

    Initial Commit

---
+++ viper_architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viper_architecture
+//
+//  Created by Ömer Faruk MERAL on 9.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
